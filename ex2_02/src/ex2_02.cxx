dd76bd29bad769693a127c500cd5061f56f9cbb5
shangguocan <shangguocan@gmail.com>
2017-04-20 10:09:48 +0800

    Add files via upload
    《医学图像编程技术》 Page14, 2.2 相机范例

---
+++ ex2_02/src/ex2_02.cxx
@@ -0,0 +1,39 @@
+#include "vtkConeSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkCamera.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+
+int main()
+{
+  vtkConeSource *cone = vtkConeSource::New();
+  cone->SetHeight( 3.0 );
+  cone->SetRadius( 1.0 );
+  cone->SetResolution( 10 );
+  
+  vtkPolyDataMapper *coneMapper = vtkPolyDataMapper::New();
+  coneMapper->SetInputConnection( cone->GetOutputPort() );
+
+  vtkActor *coneActor = vtkActor::New();
+  coneActor->SetMapper( coneMapper );
+
+  vtkRenderer *ren1= vtkRenderer::New();
+  ren1->AddActor( coneActor );
+  ren1->SetBackground( 0.1, 0.2, 0.4 );
+
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+  renWin->AddRenderer( ren1 );
+  renWin->SetSize( 300, 300 );
+
+  // 每旋转1°渲染一次，共旋转360°。
+  int i;
+  for (i = 0; i < 360; ++i)
+    {
+    renWin->Render();
+    // 获取自动创建的相机对象并将其旋转1°。
+    ren1->GetActiveCamera()->Azimuth( 1 );
+    }
+
+  return 0;
+}
